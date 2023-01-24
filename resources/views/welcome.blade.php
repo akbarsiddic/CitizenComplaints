1797c69efdf046690a657b68070cc804c81d4c87
Anntjur <rizkyakbar.rpl22020@smkn4bogor.sch.id>
2023-01-24 09:28:45 +0700

    sec commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -248,13 +248,7 @@
     <div class="container-fluid py-4">
 
         <div class="card" style="width: 18rem;">
-            <img src="../assets/img/carousel-1.jpg" class="card-img-top" alt="...">
-         <div class="card-body">
-             <h5 class="card-title">Card title</h5>
-             <h1>Hello World</h1>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
-         </div>
+            <img src="../assets/img/carousel-1.jpg" class="card-img" alt="...">
         </div>
     </div>
 
