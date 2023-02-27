57a85c95c767b6d9c195930dc895775660fe5427
AkbarSiddiq <akbarsiddiq402@gmail.com>
2023-02-27 22:29:41 +0700

    alert, pdf

--- app/Http/Controllers/LogController.php
+++ app/Http/Controllers/LogController.php
@@ -1,16 +1,51 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 use App\Models\Log;
+use barryvdh\dompdf\Facade;
+use PDF;
 
 class LogController extends Controller
 {
     //index
     public function index()
     {
-        $logs = Log::all();
+        $logs = DB::table('logs')
+            ->join('categories', 'logs.category_id', '=', 'categories.id')
+            ->join('users', 'logs.user_id', '=', 'users.id')
+            ->join('complaints', 'logs.complaint_id', '=', 'complaints.id')
+            ->select(
+                'logs.*',
+                'categories.name as category',
+                'users.name as name',
+                'complaints.description as complaint_description'
+            )
+            ->orderBy('logs.id', 'desc')
+            ->get();
+
         return view('log', compact('logs'));
+    }
+
+    public function exportToPDF()
+    {
+        $logs = DB::table('logs')
+            ->join('categories', 'logs.category_id', '=', 'categories.id')
+            ->join('users', 'logs.user_id', '=', 'users.id')
+            ->join('complaints', 'logs.complaint_id', '=', 'complaints.id')
+            ->select(
+                'logs.*',
+                'categories.name as category',
+                'users.name as name',
+                'complaints.description as complaint_description'
+            )
+            ->orderBy('logs.id', 'desc')
+            ->get();
+
+        $pdf = PDF::loadView('log-pdf', compact('logs'));
+        $pdf->setPaper('A4', 'landscape');
+
+        return $pdf->download('log.pdf');
     }
 }
