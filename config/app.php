34af6428a3e95951be23a695e72fe393055a7c95
akbarsiddic <akbarsiddiq402@gmail.com>
2023-07-10 00:00:30 +0700

    not working

--- config/app.php
+++ config/app.php
@@ -53,7 +53,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://citizen-complaints.fly.dev/'),
 
     'asset_url' => env('ASSET_URL'),
 
